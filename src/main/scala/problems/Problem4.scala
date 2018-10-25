9c92c1393879d77f583b65a52c2f1088239699a7
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-25 07:36:55 -0600

    Finished Problem 4

---
+++ src/main/scala/problems/Problem4.scala
@@ -0,0 +1,22 @@
+package problems
+
+import util.Problem
+
+/**
+  * A solution to the fourth problem of Project Euler
+  */
+object Problem4 extends Problem(4) {
+    def isPalindrome(number: BigInt): Boolean = {
+        val stringOfNumber = number.toString()
+        (0 to stringOfNumber.length / 2).forall(i => stringOfNumber.charAt(i) == stringOfNumber.charAt(stringOfNumber.length - i - 1))
+    }
+    var largestPalindrome = BigInt(0)
+    for (i <- 999 to 1 by -1) {
+        for (j <- 999 to 1 by -1) {
+            if ((i * j) > largestPalindrome && isPalindrome(i * j)) {
+                largestPalindrome = i * j
+            }
+        }
+    }
+    this.outputAnswer(largestPalindrome.toString)
+}

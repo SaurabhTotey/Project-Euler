fb8a4b5ef2d4e0ba64b620ba4e856f2b9f77c677
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-25 21:26:29 -0600

    Finished Problem 6

---
+++ src/main/scala/problems/Problem6.scala
@@ -0,0 +1,10 @@
+package problems
+
+import util.Problem
+
+/**
+  * A solution to the sixth problem of Project Euler
+  */
+object Problem6 extends Problem(6) {
+    this.outputAnswer((2L * (1L until 100).fold(0L)((sum, i) => sum + ((i + 1) to 100).fold(0L)((innerSum, j) => innerSum + i * j))).toString)
+}

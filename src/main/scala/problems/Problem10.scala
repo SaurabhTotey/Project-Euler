50117bb8023f6f0ed7787a9a1a76d0dbd6a9eed8
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-26 15:08:56 -0600

    Finished Problem 10

---
+++ src/main/scala/problems/Problem10.scala
@@ -0,0 +1,12 @@
+package problems
+
+import util.{NumberGenerator, Problem}
+
+/**
+  * A solution to the tenth problem of Project Euler
+  */
+object Problem10 extends Problem(10) {
+    val primeSeq = NumberGenerator.primeSequence()
+    primeSeq.generateWhile(() => primeSeq.top() < 2000000)
+    this.outputAnswer(primeSeq.generatedNumbers().sum - primeSeq.top())
+}

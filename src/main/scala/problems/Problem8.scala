de254375441326958dde6e929e2c75e4abcbad9a
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-25 21:50:38 -0600

    Finished Problem 8

---
+++ src/main/scala/problems/Problem8.scala
@@ -0,0 +1,11 @@
+package problems
+
+import util.Problem
+
+/**
+  * A solution to the eighth problem of Project Euler
+  */
+object Problem8 extends Problem(8) {
+    val digits = this.input.toCharArray.filter(digit => digit.isDigit).map(digit => (digit - '0').toLong)
+    this.outputAnswer((0 until digits.length - 13).map(index => digits.slice(index, index + 13).fold(1L)((product, digit) => product * digit)).max.toString)
+}

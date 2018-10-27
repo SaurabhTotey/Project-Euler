f9c1602a58b02953b6b552047e67059961e90f3b
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-26 19:08:36 -0600

    Made a script to generate files for a problem

---
+++ CreateProblem.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "Problem Number: " number
+read -p "Generate Input File (y/n): " generateInput
+filePath="src/main/scala/problems/Problem$number.scala"
+touch "$filePath"
+echo -e "package problems\n\nimport util.Problem\n\n/**\n  * A solution to the nth problem of Project Euler\n  */\nobject Problem$number extends Problem($number) {\n\n}" > "$filePath"
+if [ "$generateInput" != "${generateInput#[Yy]}" ]; then
+    touch "input/Problem$number.txt"
+fi

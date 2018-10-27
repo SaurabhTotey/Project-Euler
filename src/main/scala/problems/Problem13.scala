8017fbef997acb4b361e294cd073e28a0ede9e6f
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-27 10:24:23 -0600

    Finished Problem 13

---
+++ src/main/scala/problems/Problem13.scala
@@ -0,0 +1,10 @@
+package problems
+
+import util.Problem
+
+/**
+  * A solution to the nth problem of Project Euler
+  */
+object Problem13 extends Problem(13) {
+    this.outputAnswer(this.input.split("\n").map(line => BigInt(line)).sum.toString().substring(0, 10))
+}

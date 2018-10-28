00e99fe5193b56d64857e9cb4773cc6eecff0cf6
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-27 18:26:49 -0600

    Finished Problem 20

---
+++ src/main/scala/problems/Problem20.scala
@@ -0,0 +1,10 @@
+package problems
+
+import util.Problem
+
+/**
+  * A solution to the twentieth problem of Project Euler
+  */
+object Problem20 extends Problem(20) {
+    this.outputAnswer((BigInt(1) to BigInt(100)).product.toString().map(digit => digit - '0').sum)
+}

af33f5be47ecb1d09dc5c8d509bc35768b27616b
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-29 14:09:27 -0600

    Finished Problem 21

---
+++ src/main/scala/problems/Problem21.scala
@@ -0,0 +1,17 @@
+package problems
+
+import util.{Problem, Utility}
+
+/**
+  * A solution to the nth problem of Project Euler
+  */
+object Problem21 extends Problem(21) {
+    val factorSums = (0 until 10000).map(number => Utility.factorsOf(number).sum - number)
+    var sum = 0L
+    for (i <- 1 until 10000) {
+        if (factorSums(i) < 10000 && i != factorSums(i) && i == factorSums(factorSums(i).toInt)) {
+            sum += i
+        }
+    }
+    this.outputAnswer(sum)
+}

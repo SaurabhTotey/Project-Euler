7fc56c03f703da1f86aec62a82c0589d384ce6fd
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-29 13:27:50 -0600

    Finished Problem 19

---
+++ src/main/scala/problems/Problem19.scala
@@ -0,0 +1,23 @@
+package problems
+
+import java.util.{Calendar, GregorianCalendar}
+
+import util.Problem
+
+/**
+  * A solution to the nth problem of Project Euler
+  */
+object Problem19 extends Problem(19) {
+    val currentDate = new GregorianCalendar()
+    currentDate.set(1901, Calendar.JANUARY, 1)
+    val endDate = new GregorianCalendar()
+    endDate.set(2000, Calendar.DECEMBER, 31)
+    var count = 0
+    while (!currentDate.equals(endDate)) {
+        if (currentDate.get(Calendar.DAY_OF_MONTH) == 1 && currentDate.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+            count += 1
+        }
+        currentDate.add(Calendar.DAY_OF_YEAR, 1)
+    }
+    this.outputAnswer(count)
+}

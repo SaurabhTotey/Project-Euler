241de82f1e165f75689b18e1bab6e374ef476b0a
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-29 14:25:43 -0600

    Finished Problem 22

---
+++ src/main/scala/problems/Problem22.scala
@@ -0,0 +1,10 @@
+package problems
+
+import util.Problem
+
+/**
+  * A solution to the nth problem of Project Euler
+  */
+object Problem22 extends Problem(22) {
+    this.outputAnswer(this.input.substring(1, this.input.length - 1).split("\",\"").sorted.zipWithIndex.map({ case (name, index) => name.map(letter => (letter - 'A' + 1).toLong).sum * (index + 1) }).sum)
+}

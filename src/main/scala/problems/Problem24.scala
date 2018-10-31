b2a434688427a1ec567e52b5a2a71033c1386f3b
Saurabh Totey <SaurabhTotey@gmail.com>
2018-10-31 08:52:13 -0600

    Moved permutationsOf into Utility and reworked Problem 24 to use it

--- src/main/scala/problems/Problem24.scala
+++ src/main/scala/problems/Problem24.scala
@@ -1,17 +1,10 @@
 package problems
 
-import util.Problem
+import util.{Problem, Utility}
 
 /**
   * A solution to the twenty fourth problem of Project Euler
   */
 object Problem24 extends Problem(24) {
-    val digits = Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
-    def makePermutationFromDigits(digits: Array[String]): Array[String] = {
-        if (digits.isEmpty) {
-            return Array("")
-        }
-        digits.indices.flatMap(i => makePermutationFromDigits(digits.slice(0, i) ++ digits.slice(i + 1, digits.length)).map(permutation => digits(i) + permutation)).toArray
-    }
-    this.outputAnswer(makePermutationFromDigits(digits).sorted.apply(1000000 - 1))
+    this.outputAnswer(Utility.permutationsOf(Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")).map(permutation => permutation.mkString).sorted.apply(1000000 - 1))
 }
--- src/main/scala/util/Utility.scala
+++ src/main/scala/util/Utility.scala
@@ -1,5 +1,7 @@
 package util
 
+import scala.reflect.ClassTag
+
 /**
   * A collection of useful methods
   */
@@ -13,6 +15,19 @@ object Utility {
       */
     def factorsOf(number: Long): Array[Long] = {
         (1L to Math.sqrt(number).floor.toLong).filter(smallerNum => number % smallerNum == 0).flatMap(factor => if (number / factor == factor) Array(factor) else Array(factor, number / factor)).toArray
+    }
+
+    /**
+      * Gets all the permutations of the given list of objects
+      *
+      * @param objects The set of items to make the permutations of
+      * @return An array of all permutations of the objects array
+      */
+    def permutationsOf[T: ClassTag](objects: Array[T]): Array[Array[T]] = {
+        if (objects.length == 1) {
+            return Array(Array(objects(0)))
+        }
+        objects.indices.flatMap(i => permutationsOf(objects.slice(0, i) ++ objects.slice(i + 1, objects.length)).map(permutation => Array(objects(i)) ++ permutation)).toArray
     }
 
 }

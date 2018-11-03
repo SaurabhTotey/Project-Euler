9e709c06f376c3eac72b5806bdd2129463d1fa11
Saurabh Totey <SaurabhTotey@gmail.com>
2018-11-03 16:14:10 -0600

    Reduced amount of code needed to solve Problem 21

--- src/main/scala/problems/Problem21.scala
+++ src/main/scala/problems/Problem21.scala
@@ -7,11 +7,5 @@ import util.{Problem, Utility}
   */
 object Problem21 extends Problem(21) {
     val factorSums = (0 until 10000).map(number => Utility.factorsOf(number).sum - number)
-    var sum = 0L
-    for (i <- 1 until 10000) {
-        if (factorSums(i) < 10000 && i != factorSums(i) && i == factorSums(factorSums(i).toInt)) {
-            sum += i
-        }
-    }
-    this.outputAnswer(sum)
+    this.outputAnswer((1 until 10000).filter(i => factorSums(i) < 10000 && i != factorSums(i) && i == factorSums(factorSums(i).toInt)).sum)
 }

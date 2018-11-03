13a80010f6cbde9f11dc89071ac27eea6f3c0217
Saurabh Totey <SaurabhTotey@gmail.com>
2018-11-03 16:24:59 -0600

    Reworked Problem 179 to half its run-time

--- src/main/scala/problems/Problem179.scala
+++ src/main/scala/problems/Problem179.scala
@@ -6,5 +6,6 @@ import util.{Problem, Utility}
   * A solution to the hundred seventy ninth problem of Project Euler
   */
 object Problem179 extends Problem(179) {
-    this.outputAnswer((2 until 10000000).count(i => Utility.factorsOf(i).length == Utility.factorsOf(i + 1).length))
+    val factorCounts = (0 to 10000000).map(i => Utility.factorsOf(i).length)
+    this.outputAnswer((2 until 10000000).count(i => factorCounts(i) == factorCounts(i + 1)))
 }

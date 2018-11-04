77711e82b3d453c2a8e430f2f0d8b45cfa9e2601
Saurabh Totey <SaurabhTotey@gmail.com>
2018-11-04 10:12:48 -0700

    Made a bash script to output problems that took too long to solve

---
+++ BadlySolved.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+badlySolvedProblems=""
+for outputFileName in output/*.txt; do
+    problem=${outputFileName%".txt"}
+    problem=${problem#"output/Problem"}
+    lineNumber=0
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        ((lineNumber++))
+        if [ $lineNumber -eq 2 ]; then
+            time=${line%"ms"}
+            time=${time#"Time: "}
+            if [ $time -ge 60000 ]; then
+                badlySolvedProblems+=" $problem"
+            fi
+        fi
+    done < $outputFileName
+done
+badlySolvedProblems=$(echo $(printf "%s\n" $badlySolvedProblems | sort -n))
+echo $badlySolvedProblems

6ea1a76e23e9b6bfaff7410e022e71dd218f1f7b
Saurabh Totey <SaurabhTotey@gmail.com>
2018-11-05 16:16:13 -0700

    Finished Problem 26

---
+++ src/main/scala/problems/Problem26.scala
@@ -0,0 +1,24 @@
+package problems
+
+import util.Problem
+
+/**
+  * A solution to the twenty sixth problem of Project Euler
+  */
+object Problem26 extends Problem(26) {
+    def numberOfRepeatedDigitsFromUnitDivision(denominator: Long, currentNumerator: Long = 1, remainders: Array[Long] = Array()): Long = {
+        if (currentNumerator < denominator) {
+            return numberOfRepeatedDigitsFromUnitDivision(denominator, currentNumerator * 10, remainders)
+        }
+        val remainder = currentNumerator % denominator
+        if (remainder == 0) {
+            return 0
+        }
+        val product = currentNumerator - remainder
+        if (remainders.contains(remainder)) {
+            return remainders.length - remainders.lastIndexOf(product)
+        }
+        numberOfRepeatedDigitsFromUnitDivision(denominator, remainder, remainders ++ Array(remainder))
+    }
+    this.outputAnswer((1 until 1000).maxBy(i => numberOfRepeatedDigitsFromUnitDivision(i)))
+}

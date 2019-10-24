c0032a5e14441eed8543500f47b4163f9c7c1e46
SaurabhTotey <SaurabhTotey@gmail.com>
2019-10-24 15:24:32 -0600

    Remade bin scripts

--- bin/CreateProblem.sh
+++ bin/CreateProblem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-read -pr "Problem Number: " number
-read -pr "Generate Input File (y/n): " generateInput
+read -p "Problem Number: " -r number
+read -p "Generate Input File (y/N): " -r generateInput
 filePath="src/main/scala/problems/Problem$number.scala"
 touch "$filePath"
 echo -e "package problems\n\nimport util.Problem\n\n/**\n  * A solution to the nth problem of Project Euler\n  */\nobject Problem$number extends Problem($number) {\n\n}" > "$filePath"
